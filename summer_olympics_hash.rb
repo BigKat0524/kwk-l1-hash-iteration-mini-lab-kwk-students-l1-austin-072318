a8487355258f9ab12d57e11e3522bf2d6b35fb3b
BigKat0524 <katrinapartain.kk@gmail.com>
2018-07-26 19:05:55 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -12,7 +12,7 @@ create_olympics_hash
 def add_a_key_value_pair
    summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
   summer_olympics[:Atlanta] = "1996"
-  return summer_olympics
+   summer_olympics
 end
 def iterate_through_hash
    
